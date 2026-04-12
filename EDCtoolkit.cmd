b1e234bfc6467cbdbdb508a24ad7b1cb87014253
𝙲:\𝚄𝚜𝚎𝚛𝚜\𝚜𝚠𝚎𝚎𝚝𝚎𝚗𝚕𝚘𝚎 <225967054+sweetenloe@users.noreply.github.com>
2026-04-11 23:27:13 -0500

    Update EDCtoolkit.cmd

--- EDCtoolkit.cmd
+++ EDCtoolkit.cmd
@@ -1,6 +1,24 @@
 @echo off
 setlocal
-set "SCRIPT=%~dp0Scripts\EDCtoolkit\edctoolkit.ps1"
+
+if /I "%~1"=="/elevated" shift
+
+set "GUISCRIPT=%~dp0Scripts\EDCtoolkit\EDCtoolkit.GUI.ps1"
+set "CLISCRIPT=%~dp0Scripts\EDCtoolkit\edctoolkit.ps1"
+set "SCRIPT=%GUISCRIPT%"
+set "ISCLI=0"
+
+if /I "%~1"=="/cli" set "SCRIPT=%CLISCRIPT%" & set "ISCLI=1"
+if /I "%~1"=="-cli" set "SCRIPT=%CLISCRIPT%" & set "ISCLI=1"
+if /I "%~1"=="--cli" set "SCRIPT=%CLISCRIPT%" & set "ISCLI=1"
+
+if "%ISCLI%"=="0" (
+  net session >nul 2>&1
+  if not "%ERRORLEVEL%"=="0" (
+    powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -FilePath '%~f0' -ArgumentList '/elevated' -Verb RunAs"
+    exit /b
+  )
+)
 
 if not exist "%SCRIPT%" (
   echo [ERROR] Could not find: %SCRIPT%
